7191c8572108a9c01545db57065e335b76e01dbd
Cesar Estevez <cesarestevez04@gmail.com>
2024-10-18 00:57:42 -0500

    Completed day 1 of 100 bootcamp.

--- Python Project/pythonProject/hello.py
+++ Python Project/pythonProject/hello.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+# print("Hello World!\nHello world!\nHello World!")
+# print("Hello" + " " + "Angela")
+#
+# #This line of code will take an input using the input() function
+# print("Hello " + input("What is your name? ") + "!")
+
+#print(len(input("What is your name?")))
+
+# username = input("What is your name?")
+# length =len(username)
+# print(length)
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up?\n")
+pet =  input("What is the name of a pet?\n")
+print("Your band name could be: " + city + " " + pet)
